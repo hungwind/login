e0d39b9cada5f01ef9aebf964c126f8d30347199
hungwind <lhung6570@gmail.com>
2023-04-05 00:21:33 +0700

    first commit

---
+++ flutter_application_1/lib/b1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'MaterialApp Widget Title',
+      home: Scaffold(
+        body: Center(
+          child: Text('Material Widget'),
+        ),
+      ),
+    ),
+  );
+}
